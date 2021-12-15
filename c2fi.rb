a3f77c2bf2d736d926549906071e4f400e4ae9f7
Piero Mamberti <piero.mamberti@gmail.com>
2021-12-15 19:33:41 +0000

    Add chapter 1 examples

---
+++ c2fi.rb
@@ -0,0 +1,7 @@
+print "Hello, please enter your temperature in Celsius: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 /5) +32
+print "The Fahrenheit equivalent is "
+print fahrenheit
+puts "."
+ 
